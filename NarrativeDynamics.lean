9a1ffdaffaea25c22c8c4808e4bef1bf1c07e6e2
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 19:06:18 +0800

    build: include epistemic core module

--- NarrativeDynamics.lean
+++ NarrativeDynamics.lean
@@ -2,3 +2,4 @@ import NarrativeDynamics.Core.Drive
 import NarrativeDynamics.Core.Learning
 import NarrativeDynamics.Core.Belief
 import NarrativeDynamics.Core.Strategy
+import NarrativeDynamics.Core.Epistemic
