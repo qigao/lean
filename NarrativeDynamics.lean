c52dba54b582b28fa0af6a95abb2adb3a9e9ae4b
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 02:44:13 +0800

    feat: export grounded goal-score covariance

--- NarrativeDynamics.lean
+++ NarrativeDynamics.lean
@@ -24,3 +24,4 @@ import NarrativeDynamics.Core.EpistemicGoal
 import NarrativeDynamics.Core.EpistemicGoalCompetition
 import NarrativeDynamics.Core.GroundedEpistemicGoal
 import NarrativeDynamics.Core.GroundedGoalCovariance
+import NarrativeDynamics.Core.GroundedGoalScoreCovariance
