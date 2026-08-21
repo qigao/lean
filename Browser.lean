907ad4d1011c5bb5a513409716b0e8704ba4b7f9
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 17:11:14 +0800

    test: include feedback normalizer spec

--- Browser.lean
+++ Browser.lean
@@ -36,4 +36,5 @@ import Browser.Interaction.JournalConformanceSpec
 import Browser.Interaction.FeedbackSpec
 import Browser.Interaction.RecoverySpec
 import Browser.Interaction.RecoveryTraceSpec
+import Browser.Interaction.NormalizerSpec
 import Browser.Trace
