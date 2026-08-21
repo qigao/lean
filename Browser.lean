43ab71bdf642e9539d3ca5dbab2ef579f8173b51
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 16:47:34 +0800

    test: include feedback contract spec

--- Browser.lean
+++ Browser.lean
@@ -33,4 +33,5 @@ import Browser.Interaction.CompositionSpec
 import Browser.Interaction.TraceSpec
 import Browser.Interaction.ConformanceSpec
 import Browser.Interaction.JournalConformanceSpec
+import Browser.Interaction.FeedbackSpec
 import Browser.Trace
