b9fbbf742b4722e7212672fd02f879ec9cc20bdc
Qi Gao <qi.gao2012@gmail.com>
2026-08-23 12:13:42 +0800

    revert: preserve scoped adapter export surface

--- narrative_dynamics/adapters/__init__.py
+++ narrative_dynamics/adapters/__init__.py
@@ -5,21 +5,9 @@ from narrative_dynamics.adapters.grounded_goal import (
     GroundedGoalScenario,
     selected_goal_metrics,
 )
-from narrative_dynamics.adapters.prison_pomdp import (
-    FinitePrisonPOMDPModel,
-    create_prison_pomdp_model,
-    prison_pomdp_contract,
-    prison_pomdp_source,
-    prison_policy_metrics,
-)
 
 __all__ = [
-    "FinitePrisonPOMDPModel",
     "GroundedGoalDecisionModel",
     "GroundedGoalScenario",
-    "create_prison_pomdp_model",
-    "prison_pomdp_contract",
-    "prison_pomdp_source",
-    "prison_policy_metrics",
     "selected_goal_metrics",
 ]
