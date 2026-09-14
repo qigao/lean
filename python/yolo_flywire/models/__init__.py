f579585256b8d6e459a3076ecd0d63473d746b06
Qi Gao <qi.gao2012@gmail.com>
2026-09-14 14:03:31 +0800

    feat: export graph diagnostic classifier

--- python/yolo_flywire/models/__init__.py
+++ python/yolo_flywire/models/__init__.py
@@ -1,4 +1,4 @@
 from .gru import GRUClassifier
-from .graph_rnn import GraphRecurrentClassifier
+from .graph_rnn import GraphDiagnosticClassifier, GraphRecurrentClassifier
 
-__all__ = ["GRUClassifier", "GraphRecurrentClassifier"]
+__all__ = ["GRUClassifier", "GraphRecurrentClassifier", "GraphDiagnosticClassifier"]
