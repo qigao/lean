c59c1a8df9ece630a7f16747475554f62700ecb4
Qi Gao <qi.gao2012@gmail.com>
2026-09-15 22:42:46 +0800

    feat(lean): export temporal credit theory

--- NarrativeDynamics.lean
+++ NarrativeDynamics.lean
@@ -29,3 +29,4 @@ import NarrativeDynamics.Core.GroundedGoalRankingReversal
 import NarrativeDynamics.Core.GroundedGoalSoftmax
 import NarrativeDynamics.Core.StoryState
 import NarrativeDynamics.Core.Testimony
+import NarrativeDynamics.Core.TemporalCredit
