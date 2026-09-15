b0c5dc4d8c8ad9056b22ae619802d25d10db4cd5
Qi Gao <qi.gao2012@gmail.com>
2026-09-15 15:17:21 +0800

    feat: export fixed CX LIF reservoir

--- python/yolo_flywire/models/__init__.py
+++ python/yolo_flywire/models/__init__.py
@@ -1,4 +1,13 @@
 from .gru import GRUClassifier
 from .graph_rnn import GraphDiagnosticClassifier, GraphRecurrentClassifier
+from .cx_lif import CxDynamics, CxLifClassifier, CxLifState, CxRunStats
 
-__all__ = ["GRUClassifier", "GraphRecurrentClassifier", "GraphDiagnosticClassifier"]
+__all__ = [
+    "GRUClassifier",
+    "GraphRecurrentClassifier",
+    "GraphDiagnosticClassifier",
+    "CxDynamics",
+    "CxLifClassifier",
+    "CxLifState",
+    "CxRunStats",
+]
