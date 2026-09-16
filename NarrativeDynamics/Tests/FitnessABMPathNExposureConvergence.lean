71004a3eecb0a539e9d02236aeec8701c37faa64
Qi Gao <qi.gao2012@gmail.com>
2026-09-17 00:42:02 +0800

    feat(lean): prove exposure schedule convergence boundaries

--- NarrativeDynamics/Tests/FitnessABMPathNExposureConvergence.lean
+++ NarrativeDynamics/Tests/FitnessABMPathNExposureConvergence.lean
@@ -1,4 +1,4 @@
-import NarrativeDynamics.Core.FitnessABMPathNExposureSchedulesScratch
+import NarrativeDynamics.Core.FitnessABMPathNExposureConvergence
 
 open NarrativeDynamics
 open NarrativeDynamics.FiniteConsensus
